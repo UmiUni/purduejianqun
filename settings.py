3757650cff4d937e0a0d6bcb65ab3ef28e681f61
PKUGoodSpeed <pkulizebo@gmail.com>
2017-09-05 02:07:44 +0000

    fix remove group manager;qr advertising and refactor

--- settings.py
+++ settings.py
@@ -22,12 +22,11 @@ def init():
   v3= u"回复 2 Purdue普渡拼车总群2017🌟;\n"
   v4= u"回复 3 Purdue普渡美食总群2017🌟;\n"
   v5= u"回复 4 Purdue普渡二手车群2017🌟;\n"
-  v6= u"回复 5 Purdue普渡二手车群2017🌟;\n"
-  v7= u"回复 6 加北美CPA,REG天天刷题群;\n"
-  v8= u"回复 7 北美信用卡爱好者总群;\n"
-  v9= u"回复 8 加线上KTV开嗓🎙️北美总群;\n"
-  v10= u"回复 99 查看【北美加群小助手Jogchat.com】\n"
-  vT =v0+v00+v1+v2+v3+v4+v5+v6+v7+v8+v9+v10
+  v6= u"回复 5 加北美CPA,REG天天刷题群;\n"
+  v7= u"回复 6 北美信用卡爱好者总群;\n"
+  v8= u"回复 7 加线上KTV开嗓🎙️北美总群;\n"
+  v9= u"回复 99 查看【北美加群小助手Jogchat.com】\n"
+  vT =v0+v00+v1+v2+v3+v4+v5+v6+v7+v8+v9
   
   usersDict = {}
   admins = []
