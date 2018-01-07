18f9a8a70d7a49125d84571d0ecca0fc62b98b82
Chaoran Wang <superchaoran@gmail.com>
2018-01-07 17:48:54 +0000

    purdue

--- tiantian.py
+++ tiantian.py
@@ -37,8 +37,9 @@ def sendGroupInviteMsg(msg,CurUserName):
   if(len(x) >0):
     y= int(x[0])
     if(y>=0 and y<=9):
-      if(preventAbuseTalking(CurUserName)):
-        return
+      if msg['FromUserName'] not in settings.admins:
+        if(preventAbuseTalking(CurUserName)):
+          return
       #print settings.chatGroups[y]
       pullMembersMore(msg, settings.chatGroups[y], CurUserName)
       sleep(0.5)
